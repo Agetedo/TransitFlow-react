e3ad681d839fd6cae62f6bb0324be24198685040
Agetedo <agetedo@gmail.com>
2024-10-19 23:21:07 +0300

    update <Testimonials />

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -10,49 +10,6 @@ import "swiper/scss/navigation";
 import "swiper/scss/keyboard";
 import "..//styles/Testimonials.scss";
 
-function TestimonialsNavigation() {
-    return(
-        <div className="testimonials__navigation">
-            <div className="button-prev">
-              <img src={buttonPrev} alt="#" />
-            </div>
-            <div className="button-next">
-              <img src={buttonNext} alt="#" />
-            </div>
-        </div> 
-    );
-}
-function TestimonialsCaption() {
-    return(
-        <div className="testimonials__caption">
-            <TitleLeft 
-              caption={"Testimonials"}
-              title={"What Our Customer Say"}
-            />
-            <TestimonialsNavigation />
-        </div> 
-    );
-}
-const testimonialsList = testimonials.map(review => 
-  <SwiperSlide key={review.id} 
-    className="testimonial-slide" 
-    style={{backgroundColor: review.color}}
-  >
-    <div className="testimonial"> 
-      <section className="testimonial__caption">
-        <img className="image" src={review.imageSrc} alt="#" />
-        <div className="user">
-          <p className="user__name">{review.userName}</p>
-          <p className="user__position">{review.userPosition}</p>
-        </div>
-      </section>
-      <img className="icon" src={review.iconSrc} alt="#" />
-    </div>
-    <p className="testimonial-slide__text">{review.text}</p>
-    <img className="testimonial-slide__rating" src={review.starRating} alt="#" />
-  </SwiperSlide>
-);
-
 export default function Testimonials() {
   return (
     <Conteiner className={"testimonials"}>
@@ -82,3 +39,47 @@ export default function Testimonials() {
     </Conteiner>
   );
 }
+
+function TestimonialsNavigation() {
+  return(
+    <div className="testimonials__navigation">
+      <div className="button-prev">
+        <img src={buttonPrev} alt="#" />
+      </div>
+      <div className="button-next">
+        <img src={buttonNext} alt="#" />
+      </div>
+    </div> 
+  );
+}
+
+function TestimonialsCaption() {
+  return(
+    <div className="testimonials__caption">
+      <TitleLeft 
+        caption={"Testimonials"}
+        title={"What Our Customer Say"}
+      />
+      <TestimonialsNavigation />
+    </div> 
+  );
+}
+const testimonialsList = testimonials.map(review => 
+  <SwiperSlide key={review.id} 
+    className="testimonial-slide" 
+    style={{backgroundColor: review.color}}
+  >
+    <div className="testimonial"> 
+      <section className="testimonial__caption">
+        <img className="image" src={review.imageSrc} alt="#" />
+        <div className="user">
+          <p className="user__name">{review.userName}</p>
+          <p className="user__position">{review.userPosition}</p>
+        </div>
+      </section>
+      <img className="icon" src={review.iconSrc} alt="#" />
+    </div>
+    <p className="testimonial-slide__text">{review.text}</p>
+    <img className="testimonial-slide__rating" src={review.starRating} alt="#" />
+  </SwiperSlide>
+);
